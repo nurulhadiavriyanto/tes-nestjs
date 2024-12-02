19fb83917efe5a6785c6f379376c7f8bd9ba9b8f
BDN <nhavriyanto@gmail.com>
2024-12-02 14:52:59 +0700

    get & create with clean architecture

---
+++ src/presentations/user/user.controller.ts
@@ -0,0 +1,49 @@
+import * as bcrypt from 'bcrypt';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { UsecaseProxyModule } from 'src/infrastructures/usecase-proxy/usecase-proxy.module';
+import { UseCaseProxy } from 'src/infrastructures/usecase-proxy/usecase-proxy';
+import { GetAllUserUseCases } from 'src/applications/use-cases/user.usecase';
+import { CreateUserUseCases } from 'src/applications/use-cases/createUser.usecase';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ValidationPipe } from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  constructor(
+    @Inject(UsecaseProxyModule.GET_ALL_USERS_USE_CASE)
+    private readonly getUserUsecaseProxy: UseCaseProxy<GetAllUserUseCases>,
+    @Inject(UsecaseProxyModule.CREATE_USER_USE_CASE)
+    private readonly createUserUsecaseProxy: UseCaseProxy<CreateUserUseCases>,
+  ) {}
+
+  @Get('')
+  async getAllUsers() {
+    const result = await this.getUserUsecaseProxy.getInstance().execute();
+    return {
+      status: 'OK',
+      code: 200,
+      message: 'Get data success',
+      data: result,
+    };
+  }
+
+  @Post('/signup')
+  async createUser(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
+    const { email, name, password } = createUserDto;
+    console.log('Email:', email);
+    console.log('Name:', name);
+    console.log('Password:', password);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const result = await this.createUserUsecaseProxy.getInstance().execute({
+      email: email,
+      name: name,
+      password: hashedPassword,
+    });
+    return {
+      status: 'Created',
+      code: 201,
+      message: 'Insert data success',
+      data: result,
+    };
+  }
+}
